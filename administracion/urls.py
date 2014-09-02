510bb93bd3841e593aff5b2ad7ecff03936cc912
Vicente Abad Rodríguez <vicenteabadrodriguez@MacBook-Air-de-Vicente.local>
2014-09-02 16:34:22 +0200

    Creadas vistas para actividades

--- administracion/urls.py
+++ administracion/urls.py
@@ -1,17 +1,21 @@
 from django.conf.urls import patterns, include, url
-from administracion.views import RefugioListView, RefugioDetailView, RefugioCreate, RefugioUpdate, RefugioDelete, ActividadListView, ActividadDetailView, EjercicioListView, EjercicioDetailView 
+from administracion import views
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^$','administracion.views.get_principal'),
+    #url(r'^$login/',include(admin.site.urls),name='login'),
+    url(r'^$','administracion.views.get_principal',name='inicio'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^refugios/nuevo', RefugioCreate.as_view(),name='crear_refugio'),
-    url(r'^refugios/editar/(?P<pk>\d+)/',RefugioUpdate.as_view(),name='editar_refugio'),
-    url(r'^refugios/borrar/(?P<pk>\d+)/',RefugioDelete.as_view(),name='borrar_refugio'),
-    url(r'^refugios/(?P<pk>\d+)',RefugioDetailView.as_view(),name='detalle_refugio'),
-    url(r'^refugios/',RefugioListView.as_view(),name='lista_refugios'),
-    url(r'^ejercicios/(?P<pk>\d+)',EjercicioDetailView.as_view(), name='detalle_enunciado'),
-    url(r'^ejercicios/',EjercicioListView.as_view(), name='lista_enunciados'),
-    url(r'^actividades/(?P<pk>\d+)',ActividadDetailView.as_view(), name='detalle_actividad'),
-    url(r'^actividades/',ActividadListView.as_view(), name='lista_actividades'),    
+    url(r'^refugios/nuevo', views.RefugioCreate.as_view(),name='crear_refugio'),
+    url(r'^refugios/editar/(?P<pk>\d+)/',views.RefugioUpdate.as_view(),name='editar_refugio'),
+    url(r'^refugios/borrar/(?P<pk>\d+)/',views.RefugioDelete.as_view(),name='borrar_refugio'),
+    url(r'^refugios/(?P<pk>\d+)',views.RefugioDetailView.as_view(),name='detalle_refugio'),
+    url(r'^refugios/',views.RefugioListView.as_view(),name='lista_refugios'),
+    url(r'^ejercicios/(?P<pk>\d+)',views.EjercicioDetailView.as_view(), name='detalle_enunciado'),
+    url(r'^ejercicios/',views.EjercicioListView.as_view(), name='lista_enunciados'),
+    url(r'^actividades/nuevo', views.ActividadCreate.as_view(),name='crear_actividad'),
+    url(r'^actividades/editar/(?P<pk>\d+)/',views.ActividadUpdate.as_view(),name='editar_actividad'),
+    url(r'^actividades/borrar/(?P<pk>\d+)/',views.ActividadDelete.as_view(),name='borrar_actividad'),
+    url(r'^actividades/(?P<pk>\d+)',views.ActividadDetailView.as_view(), name='detalle_actividad'),
+    url(r'^actividades/',views.ActividadListView.as_view(), name='lista_actividades'),    
 )
